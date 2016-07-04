08c82b267428dc97b9edf7ae927f39d140710c99
_huolan <1606751797@qq.com>
2016-07-05 04:15:46 +0800

    fiel_code

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.axLicenseControl2 = new ESRI.ArcGIS.Controls.AxLicenseControl();
+            this.axToolbarControl2 = new ESRI.ArcGIS.Controls.AxToolbarControl();
+            this.axMapControl1 = new ESRI.ArcGIS.Controls.AxMapControl();
+            ((System.ComponentModel.ISupportInitialize)(this.axLicenseControl2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axToolbarControl2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axMapControl1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // axLicenseControl2
+            // 
+            this.axLicenseControl2.Enabled = true;
+            this.axLicenseControl2.Location = new System.Drawing.Point(823, -3);
+            this.axLicenseControl2.Name = "axLicenseControl2";
+            this.axLicenseControl2.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axLicenseControl2.OcxState")));
+            this.axLicenseControl2.Size = new System.Drawing.Size(32, 32);
+            this.axLicenseControl2.TabIndex = 2;
+            // 
+            // axToolbarControl2
+            // 
+            this.axToolbarControl2.Location = new System.Drawing.Point(1, 1);
+            this.axToolbarControl2.Name = "axToolbarControl2";
+            this.axToolbarControl2.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axToolbarControl2.OcxState")));
+            this.axToolbarControl2.Size = new System.Drawing.Size(265, 28);
+            this.axToolbarControl2.TabIndex = 3;
+            // 
+            // axMapControl1
+            // 
+            this.axMapControl1.Location = new System.Drawing.Point(1, 35);
+            this.axMapControl1.Name = "axMapControl1";
+            this.axMapControl1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axMapControl1.OcxState")));
+            this.axMapControl1.Size = new System.Drawing.Size(864, 379);
+            this.axMapControl1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(867, 415);
+            this.Controls.Add(this.axToolbarControl2);
+            this.Controls.Add(this.axLicenseControl2);
+            this.Controls.Add(this.axMapControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.SizeChanged += new System.EventHandler(this.Form1_SizeChanged);
+            ((System.ComponentModel.ISupportInitialize)(this.axLicenseControl2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axToolbarControl2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axMapControl1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private ESRI.ArcGIS.Controls.AxMapControl axMapControl1;
+        private ESRI.ArcGIS.Controls.AxLicenseControl axLicenseControl2;
+        private ESRI.ArcGIS.Controls.AxToolbarControl axToolbarControl2;
+    }
+}
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public int constvalue_height;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            constvalue_height = axMapControl1.Top;
+            string path = @"H:\all_chendazhen\";
+            string filename = @"all_chendazhen.shp";
+            axMapControl1.AddShapeFile(path,filename);
+            axMapControl1.Extent = axMapControl1.FullExtent;
+            
+        }
+
+        private void Form1_SizeChanged(object sender, EventArgs e)
+        {
+            axMapControl1.Width = this.Width;
+            axMapControl1.Height = this.Height - constvalue_height;
+        }
+    }
+}
